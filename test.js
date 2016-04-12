2d389f8f12aa0e1b07957b569adc7459dbdd7242
xiaolong <sss3600123@hotmail.com>
2016-04-12 17:39:02 +0800

    add func str to date

---
+++ test.js
@@ -0,0 +1,9 @@
+/**
+         * 时间格式转换成时间戳
+         * @param day
+         * @returns {number}
+         */
+        function strToTime(day){
+            var re = /(\d{4})(?:-(\d{1,2})(?:-(\d{1,2}))?)?(?:\s+(\d{1,2}):(\d{1,2}):(\d{1,2}))?/.exec(day);
+            return new Date(re[1],(re[2]||1)-1,re[3]||1,re[4]||0,re[5]||0,re[6]||0).getTime()/1000;
+        }
